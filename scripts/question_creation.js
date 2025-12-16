506f489ce32bac23d1e8354f03e984e43d7e4e24
Griffy <timeo.felix.aux@gmail.Com>
2025-12-16 15:56:13 +0100

    fixed chromonium font issues

--- scripts/question_creation.js
+++ scripts/question_creation.js
@@ -8,90 +8,88 @@ let doc_calls = {
 let question_list = JSON.parse(localStorage.getItem("current_quiz"))
 
 
-
-function render_preview(list){
+/**
+ * Edit DOM to embed questions
+ * @param {questions} list 
+ */
+function render_preview(list) {
     let temp_html_element = ""
     doc_calls.preset_view_div.innerHTML = ""
 
+    // Each element in questions root
     for (let index = 0; index < list.length; index++) {
         const element = list[index];
         temp_html_element = ""
-        
-        // create a temp string to apply it later inside a div
+
+        ///////////////////////
+        //! DOM Modification //
+        ///////////////////////
+
+        // Summon Title Inputs template
         temp_html_element = `
         <input placeholder="${element.title}" id="title_${index}" class="text-xl text-[#cfcec3] font-semibold w-full text-center max-md:text-lg mt-10"></input>`
-        for (let i = 0; i < list[index].answers.length; i++) {
-            const e = list[index].answers[i];
 
+        // Summon every Anwsers Inputs beneith the Title 
+        for (let i = 0; i < list[index].answers.length; i++) {
+            const answer = list[index].answers[i];
             temp_html_element += `
-            <input placeholder="${e.text}" id="question_button_${i.toString()}" 
-            class="question_${index.toString()} bg-[#2c2e31] m-2 max-md:bg-[#323438] shadow-2xl text-[#cfcec3] max-md:text-xs text-sm py-3 px-5 max-w-100 rounded-2xl text-center transition-all duration-500 hover:bg-amber-300 hover:text-[#323437] hover:shadow-amber-300/30">
+            <input placeholder="${answer.text}" id="question_button_${i.toString()}" class="question_${index.toString()} bg-[#2c2e31] m-2 max-md:bg-[#323438] shadow-2xl text-[#cfcec3] max-md:text-xs text-sm py-3 px-5 max-w-100 rounded-2xl text-center transition-all duration-500 hover:bg-amber-300 hover:text-[#323437] hover:shadow-amber-300/30">
             </input>`
-            
-            if (i === list[index].answers.length -1){
+            if (i === list[index].answers.length - 1) {
                 temp_html_element += `
-                <button id="add_button_${index.toString()}" class="bg-[#2c2e31] max-md:bg-[#323438] w-10 shadow-2xl text-[#cfcec3] max-md:text-sm text-lg py-1 px-2 max-w-100 rounded-2xl text-center transition-all duration-500 hover:bg-amber-300 hover:text-[#323437] hover:shadow-amber-300/30">+</button>`
+                <button id="add_button_${index.toString()}" class="bg-[#2c2e31] max-md:bg-[#323438] w-10 shadow-2xl text-[#cfcec3] max-md:text-sm text-lg py-1 px-2 max-w-100 rounded-2xl text-center transition-all duration-500 hover:bg-amber-300 hover:text-[#323437] hover:shadow-amber-300/30">
+                    +
+                </button>`
             }
         }
-        
+
+        // Wrap Title + Answers + Buttons in a div.
         doc_calls.preset_view_div.innerHTML += `<div id="question_element${index}" class="flex flex-col items-center">${temp_html_element}</div>`
-        
     }
+
     creation_part_input()
 }
 
-function creation_part_input(){
+/**
+ * Sets the even listeners using the master div childrens
+ */
+function creation_part_input() {
     let displayed_button = Array.from(doc_calls.preset_view_div.children)
-    
-    // every div child of preview div
+
     displayed_button.forEach(div_element => {
 
-        // every child of div
         for (let index = 0; index < Array.from(div_element.children).length; index++) {
             const input_element = Array.from(div_element.children)[index];
-            
-            // check for enter on <input>
-            // filter titles and buttons to edit question_list
-            // dang why does regex's were so hard to understand, never used those since ubuntu weird ahh commands 18.04
+
+            // Handle input,
+            // Filter id to locate where to edit,
+            // Apply changes
             input_element.addEventListener("keypress", (event) => {
                 if (event.key === "Enter") {
-                    
-                    if (input_element.id.includes("title_")){
-                        // filter input_id digits to locate where to edit input_element
+                    if (input_element.id.includes("title_")) {
                         question_list[input_element.id.replace(/\D/g, "")].title = input_element.value
                     }
-                    else if (input_element.id.includes("question_button_")){
-                        // filter input_id digits and his parent div to locate where to edit input_element
+                    else if (input_element.id.includes("question_button_")) {
                         question_list[input_element.parentNode.id.replace(/\D/g, "")].answers[input_element.id.replace(/\D/g, "")].text = input_element.value
                     }
 
                     render_preview(question_list)
-                    
-                    
                     localStorage.setItem("current_quiz", JSON.stringify(question_list))
                 }
             });
             input_element.addEventListener("click", (event) => {
-                if (input_element.id.includes("add_button_")){ 
-                    question_list[input_element.id.replace(/\D/g, "")].answers.push({text:"Reponse 1", value: 1,})
+                if (input_element.id.includes("add_button_")) {
+                    question_list[input_element.id.replace(/\D/g, "")].answers.push({ text: "Reponse 1", value: 1, })
 
                     render_preview(question_list)
                     localStorage.setItem("current_quiz", JSON.stringify(question_list))
                 }
-                
             })
         }
     });
 }
 
 
-
-document.addEventListener("keydown", (event) => {
-    
-    if (event.key === "k"){
-        console.log(question_list)        
-    }
-});
 
 doc_calls.reset_button.addEventListener("click", (event) => {
     console.log("test")
@@ -100,53 +98,52 @@ doc_calls.reset_button.addEventListener("click", (event) => {
         {
             title: "Example question 1",
             answers:
-            [
-                {
-                    text:"❌ Non",
-                    value: -1
-                },
-                {
-                    text:"✔️ Oui",
-                    value: 1
-                },
-                
-            ]
+                [
+                    {
+                        text: "❌ Non",
+                        value: -1
+                    },
+                    {
+                        text: "✔️ Oui",
+                        value: 1
+                    },
+
+                ]
         },
         {
             title: "Example question a trois reponses",
             answers:
-            [
-                {
-                    text:"❌Nan pour le coup",
-                    value: -1
-                },
-                {
-                    text:"✔️Bah ouais j'ai des yeux gros shlag",
-                    value: 1
-                },
-                {
-                    text:"PLaceholder de zinzin",
-                    value: 1
-                },
-            ]
+                [
+                    {
+                        text: "❌Nan pour le coup",
+                        value: -1
+                    },
+                    {
+                        text: "✔️Bah ouais j'ai des yeux gros shlag",
+                        value: 1
+                    },
+                    {
+                        text: "PLaceholder de zinzin",
+                        value: 1
+                    },
+                ]
         },
         {
             title: "Example question 2",
             answers:
-            [
-                {
-                    text: "Tellement",
-                    value: 1,
-                },
-                {
-                    text: "nan ça pue la merde",
-                    value: -1,
-                },
-            ]
+                [
+                    {
+                        text: "Tellement",
+                        value: 1,
+                    },
+                    {
+                        text: "nan ça pue la merde",
+                        value: -1,
+                    },
+                ]
         }
     ]
-    
-    // apply inputs to local storage
+
     localStorage.setItem("current_quiz", JSON.stringify(question_list))
     console.log('localStorage, edited')
     render_preview(question_list)
@@ -158,3 +155,13 @@ function sleep(ms) {
 
 
 render_preview(question_list)
+
+
+
+
+// TODO Remove after debug
+document.addEventListener("keydown", (event) => {
+    if (event.key === "k") {
+        console.log(question_list)
+    }
+});
--- scripts/question_page.js
+++ scripts/question_page.js
@@ -1,4 +1,4 @@
-let player_stats ={
+let player_stats = {
     current_score: 0
 }
 
@@ -9,89 +9,40 @@ let doc_calls =
 }
 
 let question_list = JSON.parse(localStorage.getItem("current_quiz"))
+let global_question_index = 0
 
-let question_list_bcpk = [
-    {
-        title: "un example de question ",
-        answers:
-        [
-            {
-                text:"❌ Non",
-                value: -1
-            },
-            {
-                text:"✔️ Oui",
-                value: 1
-            },
-            
-        ]
-    },
-    {
-        title: "t'avait remarquer que dans la question precedante il y avait un espace en trop ?",
-        answers:
-        [
-            {
-                text:"❌Nan pour le coup",
-                value: -10
-            },
-            {
-                text:"✔️Bah ouais j'ai des yeux gros shlag",
-                value: 11
-            }
-        ]
-    },
-    {
-        title: "un petit test vite fait pour voir si l'array arrive bien a lire",
-        answers:
-        [
-            {
-                text: "ouais ça marche !",
-                value: 10,
-            },
-            {
-                text: "nan ça pue la merde",
-                value: -19,
-            },
-        ]
-    }
-]
-
-let question_index = 0
-
-function render_questions(){
-    doc_calls.title_node.innerText = question_list[question_index].title
+// Apply data to the DOM
+function render_questions() {
     doc_calls.button_div.innerHTML = ""
-
-
-    for (let i = 0; i < question_list[question_index].answers.length; i++) {
-        const element = question_list[question_index].answers[i];
-
+    
+    doc_calls.title_node.innerText = question_list[global_question_index].title
+    for (let i = 0; i < question_list[global_question_index].answers.length; i++) {
+        const element = question_list[global_question_index].answers[i];
 
         doc_calls.button_div.innerHTML += `
         <button id="question_button_${i.toString()}" 
         class="bg-[#2c2e31] max-md:bg-[#3e4146] shadow-2xl text-[#cfcec3] max-md:text-lg text-2xl py-5 px-10 max-w-100 rounded-2xl text-center transition-all duration-500 hover:bg-amber-300 hover:text-[#323437] hover:shadow-amber-300/30">
         ${element.text}</button>`
     }
-    
+
     console.log(`finished rendering "${doc_calls.title_node.innerText}"`)
     input()
 }
 
-function input(){
+function input() {
     let displayed_button = Array.from(doc_calls.button_div.children)
 
     for (let i = 0; i < displayed_button.length; i++) {
         const element = displayed_button[i];
-        
+
         element.addEventListener("click", () => {
-            player_stats.current_score += question_list[question_index].answers[i].value
-            question_index ++
+            player_stats.current_score += question_list[global_question_index].answers[i].value
+            global_question_index++
             render_questions()
 
-            console.log(`Click sur "${question_list[question_index].answers[i].text}" ajout de ${question_list[question_index].answers[i].value} points, total : ${player_stats.current_score}`)
+            console.log(`Click sur "${question_list[global_question_index].answers[i].text}" ajout de ${question_list[global_question_index].answers[i].value} points, total : ${player_stats.current_score}`)
         })
     }
-
 }
 
 render_questions()
